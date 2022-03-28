eec87c6ad60379f2aa31f0d52d8c5a27cdba2800
Ychanho <77283665+Ychanho@users.noreply.github.com>
2022-03-28 15:20:31 +0900

    Update and rename pred_model_logger.py to neural_network_train.py

--- models/neural_network_train.py
+++ models/neural_network_train.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import TensorDataset, DataLoader
-from pred.network import SingleNet, ReshapeNet
+from model.network import SingleNet, ReshapeNet
 from datetime import datetime
 import numpy as np
 from sklearn.metrics import r2_score
