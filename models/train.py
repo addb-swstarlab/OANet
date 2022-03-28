2321063f37d273d07987d70ac7d99f1c75a0879e
Ychanho <77283665+Ychanho@users.noreply.github.com>
2022-03-28 15:23:45 +0900

    Update train.py

--- models/train.py
+++ models/train.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 import os
 # from sklearn.model_selection import StratifiedKFold
-from model.neural_network_train import NeuralModel
+from models.neural_network_train import NeuralModel
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.metrics import r2_score
